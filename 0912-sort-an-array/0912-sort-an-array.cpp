b7d5ed6cfb6f9ad6d429453c392b20d60dbf08d6
Soni Neel NikunjKumar <90117961+neel13062003@users.noreply.github.com>
2022-11-11 22:34:49 +0530

    Time: 115 ms (99.08%), Space: 57.2 MB (73.70%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
+        /*
+        https://leetcode.com/problems/sort-an-array/discuss/1401412/C%2B%2B-Clean-Code-Solution-or-Fastest-or-All-(15%2B)-Sorting-Methods-or-Detailed =>Best Sorting Discussion
+        */
         sort(nums.begin(), nums.end());
         return nums;
     }
