0c36f4e63791802bc44a2843620839cdc822c586
Soni Neel NikunjKumar <90117961+neel13062003@users.noreply.github.com>
2022-11-13 14:16:27 +0530

    Time: 11 ms (96.98%), Space: 7.6 MB (78.25%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -13,17 +13,19 @@ public:
         for(int i=1; i<=m; i++){
             for(int j=1; j<=n; j++){
                 if(word1[i-1]==word2[j-1]){
-                    dp[i][j]= 1+dp[i-1][j-1];
+                    dp[i][j]= 1+dp[i-1][j-1];                       //number is same then add with previos element
                 }
                 else{
-                    dp[i][j]= max(dp[i-1][j], dp[i][j-1]);
+                    dp[i][j]= max(dp[i-1][j], dp[i][j-1]);          //character is different so max from lefter/righter adjecents side
                 }
             }
         }
-        int x=dp[m][n];
+        int x=dp[m][n];            //returning last from dp.
         return m+n-(2*x);
         
         /*int length=lcs(word1,word2);           //lcs=longest common subsquence
         return word1.length()+word2.length()-2*length;*/
+        
+        /* Logic is simple first find longest_common_substring then from length of both string 2*dp[m][n] subsract it.*/
     }
 };
