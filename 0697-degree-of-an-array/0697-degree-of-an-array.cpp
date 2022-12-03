5b9c27d3680bf7b4f8cbe8d86cc8fbb6ef310d03
Soni Neel NikunjKumar <90117961+neel13062003@users.noreply.github.com>
2022-12-03 10:04:01 +0530

    Time: 725 ms (5.05%), Space: 25.2 MB (81.46%) - LeetHub

---
+++ 0697-degree-of-an-array/0697-degree-of-an-array.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    
+    int firstOcu(vector<int>&nums , int key){
+        int firstOc;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==key){
+                firstOc = i+1 ;
+                break;
+            }
+        }
+        return firstOc;
+    }
+    int lastOcu(vector<int>&nums , int key){
+        int lastOc;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==key){
+                lastOc = i+1 ;
+            } 
+        }
+        return lastOc;
+    }   
+    
+    int findShortestSubArray(vector<int>& nums) {
+        map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }        
+        
+        int maxf=-1;
+        for(auto it=mp.begin();it!=mp.end();it++){
+            if( (it->second) > maxf) maxf=it->second;
+        }
+        
+        vector<int> vec;
+        
+        for(auto it=mp.begin();it!=mp.end();it++){
+            if( (it->second) >= maxf ){
+                 vec.push_back(it->first);
+            }
+        }
+        int first,last,diff=-1,ans=INT_MAX; 
+        for(int i=0;i<vec.size();i++){
+             int x=vec[i];
+             first= firstOcu(nums,x);
+             last = lastOcu(nums,x);
+             diff = last-first+1;
+             if(ans>diff) ans=diff;      
+        }
+        return ans;
+    }
+};
