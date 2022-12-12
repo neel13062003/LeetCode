7cbac46500b96a2ac38a9ff4725dfddf4cdac2f1
Soni Neel NikunjKumar <90117961+neel13062003@users.noreply.github.com>
2022-12-12 19:29:52 +0530

    Time: 61 ms (39.61%), Space: 14 MB (36.67%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -6,12 +6,9 @@ public:
         vector<vector<int>> ans(n,vector<int>(m));
         
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                
+            for(int j=0;j<m;j++){  
                 int newJ=(j+k)%m; // (j + numbers of columns added)%m
-                
                 int newI=(i+(j+k)/m)%n; // (i + numbers of rows added)%n 
-                
                 ans[newI][newJ]=grid[i][j];
             }
         }
