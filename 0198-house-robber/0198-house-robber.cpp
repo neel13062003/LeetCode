4a307fe95b290902d38c1827634ecae3249268e6
Soni Neel NikunjKumar <90117961+neel13062003@users.noreply.github.com>
2022-12-14 12:44:39 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (78.33%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    int rob(vector<int>& nums,int n) {
+        if(n==0) return 0;
+        if(n==1) return nums[0];
+        return max(  rob(nums,n-1) , rob(nums,n-2)+nums[n-1]);
+    }
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        //return rob(nums,n);
+        if(n==0) return 0;
+        if(n==1) return nums[0];
+        int v1=nums[0],v2=max(v1,nums[1]);     //v2 is current
+        for(int i=2;i<n;i++){
+            int temp=v2;
+            v2=max(v2,v1+nums[i]);
+            v1=temp;
+        }
+        return v2;
+    }
+};
+
+
