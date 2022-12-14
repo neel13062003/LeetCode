770a9dc91d92289c4e1be50c374ad3306488c561
Soni Neel NikunjKumar <90117961+neel13062003@users.noreply.github.com>
2022-12-14 12:50:19 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (50.26%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,12 +2,14 @@ class Solution {
     int rob(vector<int>& nums,int n) {
         if(n==0) return 0;
         if(n==1) return nums[0];
-        return max(  rob(nums,n-1) , rob(nums,n-2)+nums[n-1]);
+        
+        //nums[n-1] means last and inside recursive is index+1
+        return max(  rob(nums,n-1) , rob(nums,n-2)+nums[n-1]);     
     }
 public:
     int rob(vector<int>& nums) {
         int n=nums.size();
-        //return rob(nums,n);
+        //return rob(nums,n);    Recursive TLE becasue it create overlappin subproblem so solvesuing dp
         if(n==0) return 0;
         if(n==1) return nums[0];
         int v1=nums[0],v2=max(v1,nums[1]);     //v2 is current
