2ba32cb4bc680fef9e622a16be8dd4aa444eb70b
Soni Neel NikunjKumar <90117961+neel13062003@users.noreply.github.com>
2022-12-16 12:19:15 +0530

    Time: 32 ms (75.30%), Space: 11.5 MB (26.35%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
      string removeDuplicates(string s) {
         string ans="";
@@ -21,4 +21,48 @@ public:
         reverse(ans.begin(),ans.end());
         return ans;
     }
+};*/
+    
+class Solution{
+    public:
+    string removeDuplicates(string s){
+         string str="";
+         stack<char>st;
+         for(int i=0;i<s.size();i++){
+            if(st.empty()){
+                st.push(s[i]);   
+            }else{
+                if(st.top()==s[i]){
+                    st.pop(); 
+                }else{
+                    st.push(s[i]);
+                } 
+            }
+         }
+         while(!st.empty()){
+            str.push_back(st.top());
+            st.pop();
+         }
+        reverse(str.begin(),str.end());
+         return str;
+    }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
