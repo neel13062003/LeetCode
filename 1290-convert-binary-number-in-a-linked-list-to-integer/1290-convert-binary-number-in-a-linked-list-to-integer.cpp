7a2b0a7f00402490d224b5c23e5f7672eb48dbbb
Soni Neel NikunjKumar <90117961+neel13062003@users.noreply.github.com>
2022-12-17 15:20:54 +0530

    Time: 3 ms (66.88%), Space: 8.4 MB (18.14%) - LeetHub

--- 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cpp
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -21,6 +21,21 @@ public:
         }
         return dec;
     }
+    /*
+    int binaryDecimal(string s){
+        int n=s.size();
+        int ans=0,base=1;
+        //int num= stoi(s);  //int(s);
+        for(int i=n-1;i>=0;i--){
+            int x=int(s[i]);
+            ans=ans+ x*base;
+            base = base*2;
+        }
+        return ans;    
+    }*/
+    
+    
+    
     
     int getDecimalValue(ListNode* head) {
        string s;
