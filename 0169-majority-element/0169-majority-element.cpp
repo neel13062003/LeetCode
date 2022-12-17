9ca56c6bdab0b8f5e073fdfa4271368b09bc8148
Soni Neel NikunjKumar <90117961+neel13062003@users.noreply.github.com>
2022-12-17 14:50:18 +0530

    Time: 23 ms (85.09%), Space: 19.7 MB (15.66%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,8 +3,7 @@ public:
     int majorityElement(vector<int>& nums) {
        map<int,int>mp; int ans; int n=nums.size()/2; int maxi=INT_MIN;
        for(int i=0;i<nums.size();i++) mp[nums[i]]++;
-       for(auto it=mp.begin();it!=mp.end();it++){
-           if(it->second >=n   &&  maxi< it->second) ans=it->first; maxi=it->second;} 
+       for(auto it=mp.begin();it!=mp.end();it++){ if(it->second >=n   &&  maxi< it->second) ans=it->first; maxi=it->second;} 
        return ans; 
     }
 };
