0828b81e574923e25ffc9be9384580da040a4771
Soni Neel NikunjKumar <90117961+neel13062003@users.noreply.github.com>
2022-12-20 11:57:31 +0530

    Time: 21 ms (29.39%), Space: 13.6 MB (85.52%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,10 +1,63 @@
 class Solution {
 public:
+    
+    int lastOcc(vector<int>&nums,int n,int key){
+        int start=0;
+        int end=n-1;
+        int mid=start+(end-start)/2;
+        int ans=-1;
+        while(start<=end){
+            if(nums[mid]==key){
+                ans=mid;
+                start=mid+1;
+            }else if(nums[mid]>key){
+                end=mid-1;
+            }else if(nums[mid]<key){
+                 start=mid+1;
+            }
+            
+            mid= start + (end-start)/2;
+        }
+        return ans;
+    }
+    
+    
+    int firstOcc(vector<int>&nums,int n,int key){
+        int start=0;
+        int end=n-1;
+        int mid=start+(end-start)/2;
+        int ans=-1;
+        while(start<=end){
+            if(nums[mid]==key){
+                ans=mid;
+                 end=mid-1;
+            }else if(nums[mid]>key){
+                 end=mid-1;
+            }else if(nums[mid]<key){
+                start=mid+1;
+            }
+            mid= start + (end-start)/2;
+        }
+        return ans;
+    }
+    
+
     vector<int> searchRange(vector<int>& nums, int target) {
         //First Ocu= UP Bound & Last Ocu = LB
         
         
         
+        //Using Binary Search
+        pair<int,int>p;
+        int n=nums.size();
+        p.first= firstOcc( nums,n,target );
+        p.second= lastOcc( nums,n,target);
+        return {p.first,p.second};
+        
+         
+        
+        
+        /*
         //1) Using Linear Search
         vector<int>ans;
         //pair<int,int> p;
@@ -24,8 +77,7 @@ public:
         ans.push_back(p);
         return ans;
         
-        /*
-         /*vector<pair<int,int>> vec;
+        vector<pair<int,int>> vec;
         vec.push_back(  {p.first,p.second} );
         return vec;
         return {p.first,p.second}; */
