e316839862d2a1c719dc6cb61e3c42fdc3653d58
Soni Neel NikunjKumar <90117961+neel13062003@users.noreply.github.com>
2022-12-22 10:43:56 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (67.20%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -9,6 +9,8 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+//Recursive
+/*
 class Solution {
 public:
     
@@ -27,4 +29,44 @@ public:
         printPO(root,ans);
         return ans;
     }
+};*/
+
+//Iterative
+//Using 2 Stack
+class Solution {
+public:    
+    //left right root
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int>postorder;
+        if(root==NULL) return postorder;
+        stack<TreeNode*>st1,st2;
+        st1.push(root);
+        while(!st1.empty()){
+            root=st1.top();
+            st1.pop();
+            st2.push(root);
+            if(root->left!=NULL){
+                  st1.push(root->left);  
+            }
+            if(root->right!=NULL){
+                st1.push(root->right);
+            }
+        }
+        while(!st2.empty()){
+            postorder.push_back(st2.top()->val);
+            st2.pop();
+        }
+        return postorder;
+    }
 };
+
+
+
+
+
+
+
+
+
+
+
