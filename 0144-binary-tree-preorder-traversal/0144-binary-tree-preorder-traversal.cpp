3861d41c96c6352650add317e962b64c6e3d11c5
Soni Neel NikunjKumar <90117961+neel13062003@users.noreply.github.com>
2022-12-22 10:19:56 +0530

    Time: 3 ms (61.81%), Space: 8.5 MB (42.98%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -9,6 +9,8 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// Recursive
+/*
 class Solution {
 public:
     
@@ -26,4 +28,41 @@ public:
         printPreorder(root,ans);
         return ans;
     }
+};*/
+//Root- Left Right
+//Iterative= > Using Stack
+class Solution {
+public:
+    //preorder => root left right
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int>preorder;
+        if(root==NULL)return preorder;
+        
+        stack<TreeNode*>st;
+        st.push(root);
+        
+        while(!st.empty()){
+            root=st.top();
+            st.pop();
+            preorder.push_back(root->val);
+            if(root->right!=NULL){
+                st.push(root->right);
+            }
+            if(root->left!=NULL){
+                st.push(root->left);
+            }            
+        }
+        return preorder;
+    }
 };
+
+
+
+
+
+
+
+
+
+
+
