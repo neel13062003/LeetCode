8f9aff53396cc14519eff1243d1e1812f0a67a7b
Soni Neel NikunjKumar <90117961+neel13062003@users.noreply.github.com>
2023-01-07 12:25:18 +0530

    Time: 7 ms (84.80%), Space: 13 MB (62.93%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,25 @@
+//First Problem is about only chhose maximum 1 profit and give result in O(N)
+//Striver All 6 Parts Are Amazing
+
+class Solution {
+public:
+    int maxProfit(vector<int>& a) {
+        int sum=0;  //a =prices
+        for(int i=1;i<a.size();i++){
+            int t=a[i]-a[i-1];
+            if(t>0)
+            sum+=t;
+        }
+        return sum;
+    }
+};
+
+//O(n) = Time complexity
+//O(1) = Space Complexity
+
+//It works because You only need to When you have on stock in your hand until you can't sell this stock.
+
+//Otherwise this testcase might give wrong 
+
+// 3 4 5 15 => This will return 12 but for this prevois constraintpurchases we can't purchase new stock until we sold our prevois stock.
+// 3 4 5 15 => This will return 13 for if we holding stock and agiain purchase
