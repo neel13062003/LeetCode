d24dd1897b367641c9b28fc087480f8a3a9b76fe
Soni Neel NikunjKumar <90117961+neel13062003@users.noreply.github.com>
2023-01-07 12:09:38 +0530

    Time: 139 ms (85.25%), Space: 93.4 MB (53.96%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,26 @@
+/*https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv/discuss/638137/All-Variants-of-%22Best-Time-to-Buy-and-Sell-Stock%22-problems*/
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        /*TLE =  O(n^2)
+        int maxi = 0;
+        for(int i=0;i<prices.size();i++){
+            for(int j=i+1;j<prices.size();j++){
+                if(prices[j]-prices[i] > maxi){
+                    maxi = prices[j]-prices[i];
+                }
+            }
+        }
+        return maxi;*/
+        
+        //O(n)
+        int mini=prices[0];
+        int cost,profit=0;
+        for(int i=1;i<prices.size();i++){
+            cost = prices[i]-mini;
+            profit =  max( profit,cost);
+            mini= min(mini,prices[i]);
+        }
+        return profit;
+    }
+};
