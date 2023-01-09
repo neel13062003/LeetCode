122c9b1cfc52bdf1c5714becdcf49cf8efd7df45
Soni Neel NikunjKumar <90117961+neel13062003@users.noreply.github.com>
2023-01-09 18:15:15 +0530

    Time: 3 ms (35.50%), Space: 5.8 MB (99.26%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        n=n+2;
-        int n1=0,n2=1,temp;
-        for(int i=0;i<n-2;i++){
-            temp=n1+n2;
-            n1=n2;
-            n2=temp;
+        int dp[10000];
+        if(n==0){
+            return 1;
+        }else if(n==1){
+            return 1;
         }
-        return temp;
+        dp[0]=1,dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
