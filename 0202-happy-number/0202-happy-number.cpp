05591d1329bc1adb1c2b0f483e02fde86aecc3d0
Soni Neel NikunjKumar <90117961+neel13062003@users.noreply.github.com>
2023-01-23 13:43:42 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (72.99%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,26 +1,5 @@
 class Solution {
 public:
-    
-   /* int digitsum(int n){
-        int sum=0;
-        while(n>0){
-            int temp=n%10;
-            sum=sum + temp*temp;
-            n=n/10;
-        }
-        return sum;
-    }
-    
-    bool isHappy(int n) {
-        int p=0;
-        while(n>0){
-            if(n==1) p=1; break;
-            n=digitsum(n);
-        }
-        if(p==1) return true;
-        else return false;
-    }*/
-    
     bool isHappy(int n){
         if(n == 1 || n == 7) return true;
         else if(n < 10) return false;
