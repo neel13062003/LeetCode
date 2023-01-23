c2a96f441552852a6f376cd99f8961d2b9b87d52
Soni Neel NikunjKumar <90117961+neel13062003@users.noreply.github.com>
2023-01-23 13:29:54 +0530

    Time: 8 ms (85.26%), Space: 6 MB (60.48%) - LeetHub

---
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+        /*int ans=left;
+        for(int i=left+1;i<=right;i++){
+            ans = ans & i;
+        }
+        return ans;*/
+        
+        
+        // if both the numbers left and right is same, return anyone
+        if(left==0 || right ==0) return 0;
+
+        // if there binary numbers for left and right are of different digits
+        if((int)log2(left)!=(int)log2(right)) return 0; 
+        
+        //initialize output with left range
+        int res = left;
+
+        while(left<right){
+            res &= ++left;
+        }
+        return res;
+        
+        
+    }
+};
