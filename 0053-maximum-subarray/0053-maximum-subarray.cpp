71aa8117a9cef61563cfde69c5ff21e3092dc941
Soni Neel NikunjKumar <90117961+neel13062003@users.noreply.github.com>
2023-01-25 23:18:10 +0530

    Time: 156 ms (32.45%), Space: 70.3 MB (7.53%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    
+    //Contigous SubArray.
+    int maxSubArray(vector<int>& nums) {
+            vector<int>dp(nums.size());
+            int ans=nums[0];
+            dp[0]=nums[0];
+        
+            for(int i=1;i<nums.size();i++){
+                dp[i]= max(nums[i],nums[i]+dp[i-1]);
+                ans=max(ans,dp[i]);
+            }
+            return ans;
+    }
+};
