802be3725a9e0af09b2be9656e12a6c4afa764fd
Soni Neel NikunjKumar <90117961+neel13062003@users.noreply.github.com>
2023-01-26 21:38:22 +0530

    Time: 25 ms (39.08%), Space: 13.8 MB (30.32%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,66 +1,27 @@
 class Solution {
 public:
+    
+    static bool cmp(pair<int,int>&A,pair<int,int>&B){
+        return A.second>B.second;
+    }
+    
     vector<int> topKFrequent(vector<int>& nums, int k) {
-    /*vector<int>answer;
-    unordered_map<int,int>m;
-	for(int i=0;i<nums.size();i++)
-        m[nums[i]]++;
-    
-    priority_queue<pair<int,int>>pq;
-    for(auto x:m)
-    {
-    pq.push({-x.second,x.first});//work as min heap
-    if(pq.size()>k)
-        pq.pop();
+         map<int,int>mp;
+         for(int i=0;i<nums.size();i++){
+             mp[nums[i]]++;
+         }
+         vector<int>ans;
+         int count=0;
+         vector<pair<int,int>>vec;
+         for(auto it=mp.begin();it!=mp.end();it++){
+             vec.push_back({it->first,it->second});
+         }
+         sort(vec.begin(),vec.end(),cmp);
+        
+         for(int i=0;i<k;i++){
+             ans.push_back(vec[i].first);
+         }
+         
+        return ans;
     }
-    
-    while(!pq.empty())
-    {
-        answer.push_back(pq.top().second);
-        pq.pop();
-    }
-    
-    return answer;*/
-        /*
-        map<int,int>mp;
-        vector<int>ans;
-        for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
-        }
-        for(auto it=mp.begin();it!=mp.end();it++){
-            if( (it->second) >=  k){
-                ans.push_back(it->first);
-            }
-        }
-        return ans;*/
-        
-        int n = nums.size();
-        map<int,int> m;
-        vector<int> ans;
-        
-        for(int i=0;i<n;i++)
-        {
-            m[nums[i]]++;
-        }
-        
-        priority_queue<pair<int,int>> pq;
-        
-        for(auto x : m)
-        {
-            pq.push({x.second,x.first});
-        }
- 
-        while(k--)
-        {
-            ans.push_back(pq.top().second);
-            pq.pop();
-        }
-       return ans; 
-        
-        //K top k frequent doesn't means etla j. Top ni frequceny vala total k elements.
-        
-        
-}
-   
-    
 };
