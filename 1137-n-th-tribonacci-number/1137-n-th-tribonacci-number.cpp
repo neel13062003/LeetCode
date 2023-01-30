7f46e26d96402b62543d30166d72e6267b520fc1
Soni Neel NikunjKumar <90117961+neel13062003@users.noreply.github.com>
2023-01-30 13:23:40 +0530

    Time: 0 ms (100.00%), Space: 6 MB (37.24%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -3,8 +3,8 @@ public:
     int tribonacci(int n) {
         /*if(n==0) return 0;
         if(n==1 || n==2) return 1;
-        return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3); */
-        if(n==0) return 0;
+        return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);*/
+         if(n==0) return 0;
         int n1=0,n2=1,n3=1,temp;
         for(int i=0;i<n-2;i++){
             temp=n1+n2+n3;
@@ -13,5 +13,6 @@ public:
             n3=temp;
         }
         return n3;
+       
     }
 };
