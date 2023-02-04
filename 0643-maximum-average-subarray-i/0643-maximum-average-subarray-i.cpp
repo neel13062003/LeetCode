f69ce8c64a2dab86840e41467abaa833c482e64d
Soni Neel NikunjKumar <90117961+neel13062003@users.noreply.github.com>
2023-02-04 15:50:49 +0530

    Time: 221 ms (32.01%), Space: 109.7 MB (68.52%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -1,26 +1,55 @@
 class Solution {
 public:
-    double findMaxAverage(vector<int>& nums, int k)
-    {
-       long int s=0;
-       long int m = INT_MIN;
-       int i=0,j=0;
-       while(j<nums.size())
-       {
-           s += nums[j];
-           if(j-i+1<k)
-           {
-               j++;
-           }
-           else if(j-i+1==k)
-           {
-              m = max(s, m);
-              s -= nums[i];
-              i++;
-              j++;
-           }
-       }
-         double avg = (double)m/(double)k;
-       return avg;
+    
+    /*double max(double a,double b){
+        return a>b ? a:b;
+    }*/
+    
+    double findMaxAverage(vector<int>& nums, int k){
+        double maxValue = 0; 
+        for(int i = 0 ; i < k ; i++)
+            maxValue += nums[i] ;
+
+        int start = 0;
+        double curValue = maxValue; 
+        for(int i = k ; i < nums.size() ; i++)
+        {
+            curValue = curValue - nums[start] + nums[i]; 
+            maxValue = max(curValue, maxValue);
+            start++ ; 
+        }  
+        return maxValue/k ;
     }
 };
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*vector<int> pref=nums;
+        long long mx= LLONG_MIN;
+        if(k==1) mx=nums.front();
+        for(int i=1;i<nums.size();i++){
+            pref[i]+=pref[i-1];
+            if(i>=k-1){
+                long long delta=pref[i]-pref[i-k+1]+nums[i-k+1];
+                mx=max(mx,delta);
+            }
+        }
+        double mean=(mx*1.0)/k;
+        return mean;*/
