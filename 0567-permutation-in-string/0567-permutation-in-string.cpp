4b2ff9f705f9a880a2d90944263862c6e035184f
Soni Neel NikunjKumar <90117961+neel13062003@users.noreply.github.com>
2023-02-04 12:29:04 +0530

    Time: 9 ms (68.05%), Space: 7.4 MB (45.48%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,19 +1,34 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        //vector<int> temp=vector(26,0);
+        
+        //Take Vector and Intialise Value With 0
         vector<int>temp(26,0);
+        
+        //Find Size Of Both String
         int len1 = s1.size(), len2 = s2.size();
+        
+        //If Size of First String Greater than it impossible to find permutation in string s2        
         if(len1>len2) return false;
+        
         vector<int> str1 = temp;
         vector<int> str2 = temp;
+        
+        //Store Frequency Of String s1 in vector=>str1
         for(auto& ch : s1) str1[ch-'a']++;
+        
+        //Use Sliding Winodw Concept 
+        //& Size of Sliding Window Size is s1.size().
         int start=0,end=0;
         while(end<len2){
             str2[s2[end++]-'a']++;
+            
+            //if frequency and Character of sliding window in s2 string is equal with string s1
             if(str1==str2) return true;
+            
             if(end>=len1) str2[s2[start++]-'a']--;
         }
+        
         return false;
     }
 
