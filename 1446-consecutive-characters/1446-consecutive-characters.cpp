7a012fa584fee2ed2ff3abfcecf77b60a9ae1569
Soni Neel NikunjKumar <90117961+neel13062003@users.noreply.github.com>
2023-02-10 12:26:33 +0530

    Time: 3 ms (77.37%), Space: 6.9 MB (30.63%) - LeetHub

--- 1446-consecutive-characters/1446-consecutive-characters.cpp
+++ 1446-consecutive-characters/1446-consecutive-characters.cpp
@@ -4,17 +4,17 @@ public:
         if(s.size()==1){
             return 1;
         }else{
-             int count=1;
-        int maxi=0;
-        for(int i=1;i<s.size();i++){
-            if(s[i]==s[i-1]){
-                count++;
-            }else{
-                count=1;
+            int count=1;
+            int maxi=0;
+            for(int i=1;i<s.size();i++){
+                if(s[i]==s[i-1]){
+                    count++;
+                }else{
+                    count=1;
+                }
+                maxi=max(count,maxi);
             }
-            maxi=max(count,maxi);
-        }
-        return maxi;
+            return maxi;
         }
        
     }
