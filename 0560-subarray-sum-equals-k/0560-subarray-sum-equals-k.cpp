d8290cc5137b4d868a7997c90efc9f62c16e40a4
Soni Neel NikunjKumar <90117961+neel13062003@users.noreply.github.com>
2023-03-12 17:45:59 +0530

    Time: 97 ms (62.85%), Space: 41.4 MB (85.92%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int,int>mp;
-        int sum=0;
-        int count=0;
-        for(int i=0;i<nums.size();i++){
-            sum+=nums[i];
-            if(sum==k){
-                count++;
+        unordered_map<int, int> sum_count; // map to store count of running sums
+        int count = 0; // count of subarrays with sum k
+        int sum = 0; // running sum
+        
+        sum_count[0] = 1; // initialize the count of running sum 0 as 1
+        
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+            if (sum_count.find(sum - k) != sum_count.end()) { // if a running sum seen earlier has a difference of k with the current running sum
+                count += sum_count[sum - k]; // add the count of subarrays with sum k that end at the earlier index
             }
-            if(mp.find(sum-k)!=mp.end()){
-                count+= mp[sum-k];
-            }
-            mp[sum]++;
+            sum_count[sum]++; // increment the count of the current running sum
         }
+        
         return count;
     }
 };
