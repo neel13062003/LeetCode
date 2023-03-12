967c921a22d01299fc9d4393535b6671d1089efd
Soni Neel NikunjKumar <90117961+neel13062003@users.noreply.github.com>
2023-03-12 17:47:41 +0530

    Time: 104 ms (19.34%), Space: 61.3 MB (32.21%) - LeetHub

---
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        if (k <= 1) { // special case when k is 0 or 1
+            return 0;
+        }
+        int count = 0;
+        int left = 0; // left index of sliding window
+        int product = 1; // product of elements in sliding window
+        
+        for (int right = 0; right < nums.size(); right++) {
+            product *= nums[right]; // expand the sliding window by multiplying the current element
+            while (product >= k) { // shrink the sliding window until the product is less than k
+                product /= nums[left];
+                left++;
+            }
+            count += (right - left + 1); // count the number of subarrays that end at the current index
+        }
+        return count;
+    }
+};
+
