425c3d66ce7e3504c152c9ba335271b38b1d0ac4
Soni Neel NikunjKumar <90117961+neel13062003@users.noreply.github.com>
2023-03-29 19:46:50 +0530

    Time: 661 ms (7.77%), Space: 10.3 MB (73.79%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
-    
-    static bool cmp(pair<int,int>&A,pair<int,int>&B){
-        return A.first<B.first;
-    }
-    
     vector<int> twoSum(vector<int>& nums, int target) {
-        int left=0;
-        int right=nums.size()-1;
         vector<int>ans;
-        vector<pair<int,int>>vec;
-        for(int i=0;i<nums.size();i++){
-            vec.push_back({nums[i],i});
-        }
-        sort(vec.begin(),vec.end(),cmp);
         
-        while(left<=right){
-            if( vec[left].first + vec[right].first == target ){
-                 ans.push_back(vec[left].second);
-                 ans.push_back(vec[right].second);
-                 break;
-            }else if( vec[left].first+vec[right].first > target){
-                 right--;
-            }else{
-                 left++;
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]+nums[j]==target){
+                    ans.push_back(i);
+                    ans.push_back(j);
+                    break;
+                }
             }
         }
+        
         return ans;
     }
 };
