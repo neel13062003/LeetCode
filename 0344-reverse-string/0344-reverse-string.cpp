c01ab3f85a3dafdfe60cdf6d2d84e4c1c6216240
Soni Neel NikunjKumar <90117961+neel13062003@users.noreply.github.com>
2023-04-02 02:21:02 +0530

    Time: 19 ms (82.32%), Space: 23.3 MB (50.93%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        reverse(s.begin(),s.end());
+        //reverse(s.begin(),s.end());
+        int start=0,end = s.size()-1;
+        while(start<end){
+            char temp  = s[start];
+            s[start]=s[end];
+            s[end]= temp;
+            start++;
+            end--;
+        }
     }
 };
