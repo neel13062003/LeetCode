a1b491469a324d2379679640bd86dc8413290a36
Soni Neel NikunjKumar <90117961+neel13062003@users.noreply.github.com>
2023-04-04 12:40:45 +0530

    Added solution - LeetHub

--- Equal Sum and XOR - GFG/equal-sum-and-xor.cpp
+++ Equal Sum and XOR - GFG/equal-sum-and-xor.cpp
@@ -24,15 +24,22 @@ int main()
 /*You are required to complete this method */
 int countValues (int n)
 {
-    int countV = 0;
+    // int countV = 0;
  
-    // Traverse all numbers from 0 to n and
-    // increment result only when given condition
-    // is satisfied.
-    for (int i=0; i<=n; i++ )
-        if ((n+i) == (n^i) )
-            countV++;
+    // // Traverse all numbers from 0 to n and
+    // // increment result only when given condition
+    // // is satisfied.
+    // for (int i=0; i<=n; i++ )
+    //     if ((n+i) == (n^i) )
+    //         countV++;
  
-    return countV;
+    // return countV;
+    int unset_bits=0;
+    while(n){
+        if((n&1)==0)
+            unset_bits++;
+        n=n>>1;
+    }
+    return 1<<unset_bits;
 }
  
