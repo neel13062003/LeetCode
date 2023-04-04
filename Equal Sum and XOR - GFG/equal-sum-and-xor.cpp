6efe63119f52279c2e6fd6d7bec9345c4cfc6b1a
Soni Neel NikunjKumar <90117961+neel13062003@users.noreply.github.com>
2023-04-04 12:39:44 +0530

    Added solution - LeetHub

---
+++ Equal Sum and XOR - GFG/equal-sum-and-xor.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+#include <iostream>
+using namespace std;
+ 
+
+int countValues (int n);
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		
+		cout<<countValues(n)<<endl;
+	}
+}
+// } Driver Code Ends
+
+
+/*You are required to complete this method */
+int countValues (int n)
+{
+    int countV = 0;
+ 
+    // Traverse all numbers from 0 to n and
+    // increment result only when given condition
+    // is satisfied.
+    for (int i=0; i<=n; i++ )
+        if ((n+i) == (n^i) )
+            countV++;
+ 
+    return countV;
+}
+ 
