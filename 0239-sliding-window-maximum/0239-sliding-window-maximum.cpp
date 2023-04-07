8bc015310e2e6b16008d3da030781342cbd052c8
Soni Neel NikunjKumar <90117961+neel13062003@users.noreply.github.com>
2023-04-07 14:55:46 +0530

    Time: 288 ms (80.46%), Space: 134.7 MB (66.28%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,39 +1,21 @@
-#include <bits/stdc++.h>
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& arr, int k) {
-        int n=arr.size();
-        list<int> l;
-        vector<int> ans; int i = 0, j=0;
-        while(j < n)
-        {
-            while(l.size() > 0 && l.back() < arr[j])     //parallery check maximum and store maximum
-                l.pop_back();       //if not maximum pop
-            
-            l.push_back(arr[j]); //if maximum insert into list
-            
-            if(j - i + 1 < k)         //haji sliding window sudhi nathi pochya
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int>q;
+        int n=nums.size();
+        int i=0,j=0;
+        vector<int>res;
+        while(j<n){
+            while(!q.empty() && q.back()<nums[j]) q.pop_back();
+            q.push_back(nums[j]);
+            if(j-i+1<k) j++;
+            else if(j-i+1==k){
+                res.push_back(q.front());
+                if(q.front()==nums[i]) q.pop_front();
+                i++;
                 j++;
-            else if(j - i + 1 == k)
-            {
-                ans.push_back(l.front());
-                if(l.front() == arr[i])    // first ne remove
-                    l.pop_front();
-                i++; j++;                //sliding window karva
             }
         }
-        
-        return ans;
-        
-        /*vector<int>ans;
-         int maxF= INT_MIN;
-        for(int i=0;i<nums.size()-k+1;i++){
-            for(int j=i;j<k;j++){
-                  maxF=max(maxF,nums[j]);
-            } 
-            ans.push_back(maxF);
-            maxF=INT_MIN;
-        }
-        return ans;*/
+        return res;
     }
 };
