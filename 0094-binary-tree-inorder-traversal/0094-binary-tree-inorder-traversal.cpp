294ca30996c52466e6a468fac17291471564a1ce
Soni Neel NikunjKumar <90117961+neel13062003@users.noreply.github.com>
2023-04-08 06:37:57 +0530

    Time: 0 ms (100.00%), Space: 9.7 MB (12.22%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -9,75 +9,22 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-/*class Solution {
+class Solution {
 public:
-    vector<int>ans;  //Must Be Declare Outside
+    
+    vector<int>ans;
     vector<int> inorderTraversal(TreeNode* root) {
-        if(!root) return ans;
-        inorderTraversal(root->left);
-        ans.push_back(root->val);
-        inorderTraversal(root->right);
+       
         
-        return ans;
-    }
-};*/
-
-
-//Recursive
-/*
-class Solution{
-    public:
-    
-    void PrintInorder(TreeNode* root,vector<int>&ans){
-        if(!root){
-            return;
-        }
-        PrintInorder(root->left,ans);
-        ans.push_back(root->val);
-        PrintInorder(root->right,ans);
-    }
-    
-    vector<int>inorderTraversal(TreeNode* root){
-        vector<int>ans;
-        PrintInorder(root,ans);
-        return ans;
+       if(root==NULL)return{}; 
+        
+       inorderTraversal(root->left);
+       ans.push_back(root->val); 
+       inorderTraversal(root->right);
+        
+       return ans; 
     }
 };
-*/
-    
-//Iterative using stack
-//Inorder=  left root right    
-class Solution{
-public:
-    
-    vector<int>inorderTraversal(TreeNode* root){
-        vector<int>inorder;
-        stack<TreeNode*>st;
-        TreeNode* node=root;
-        while(true){
-            if(node!=NULL){
-                st.push(node);
-                node=node->left;
-            }else{
-                if(st.empty())break;
-                node=st.top();
-                st.pop();
-                inorder.push_back(node->val);
-                node=node->right;
-            }
-        }
-        return inorder;
-    }
-};
-
-
-
-
-
-
-
-
-
 
 
 
