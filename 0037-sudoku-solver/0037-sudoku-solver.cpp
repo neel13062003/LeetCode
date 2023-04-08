7e0425a58080f9bcda7d8255d032e36988d476ae
Soni Neel NikunjKumar <90117961+neel13062003@users.noreply.github.com>
2023-04-08 07:22:01 +0530

    Time: 42 ms (34.36%), Space: 6.3 MB (82.23%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -8,11 +8,12 @@ public:
                         if(isValid(col,row,c,board)){
                             board[row][col]=c;
                             if(solve(board)==true)
-                            return true;
+                                return true;
                             else
-                            board[row][col]='.';
+                                board[row][col]='.';
                         }
-                    }return false;
+                    }
+                    return false;
                 }
             }
         }return true;
@@ -27,3 +28,33 @@ public:
         solve(board);
     }
 };
+
+
+/*
+
+Explaination
+This line of code is checking whether the current value 'c' can be placed in the current cell (row, col) of the Sudoku board. Specifically, it is checking whether 'c' is already present in the 3x3 sub-grid that contains the current cell.
+
+Here's how this line of code works:
+
+Sudoku board consists of a 9x9 grid, which is divided into 9 3x3 sub-grids.
+
+For a given cell (row, col), we can determine which sub-grid it belongs to using the following formula:
+
+subgrid_row = 3 * (row / 3)
+subgrid_col = 3 * (col / 3)
+
+This formula calculates the row and column indices of the top-left cell of the sub-grid that contains the current cell.
+
+Once we know the indices of the sub-grid, we can iterate over all 9 cells in the sub-grid using the variable 'i'.
+
+The index 'i' can be divided into two parts: 'i/3' gives the row index relative to the top-left cell of the sub-grid, and 'i%3' gives the column index relative to the top-left cell of the sub-grid.
+
+Finally, we can check whether the value 'c' is already present in the current cell of the sub-grid using the following code:
+
+if(board[3*(row/3)+i/3][3*(col/3)+i%3]==c) return false;
+
+This code uses the formula we derived earlier to calculate the row and column indices of the current cell in the sub-grid, and then checks whether its value is equal to 'c'.
+
+If 'c' is already present in the sub-grid, the function returns false, indicating that 'c' cannot be placed in the current cell. Otherwise, it continues with the other checks for the row and column.
+*/
