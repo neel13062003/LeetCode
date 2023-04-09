7635b9859379974c0f8a9a738f3192b52c0146bb
Soni Neel NikunjKumar <90117961+neel13062003@users.noreply.github.com>
2023-04-09 18:12:17 +0530

    Time: 23 ms (79.39%), Space: 15.1 MB (30.83%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,37 +1,76 @@
 class Solution {
-    
 private:
-    void dfs(vector<int>adjList[],int vis[],int node){
+  void dfs(int node,vector<int>adj[],vector<int>&vis){
         vis[node]=1;
-        for(auto it:adjList[node]){
-            if(!vis[it]){
-                dfs(adjList,vis,it);
-            }
+        for(auto it : adj[node]){
+             if(!vis[it]){
+                   dfs(it,adj,vis);
+             }
         }
-    }
-    
+  }
 public:
+     //Number of Total Connected Component
     int findCircleNum(vector<vector<int>>& isConnected) {
-        
-        int V= isConnected.size();
-        //1) Convert Adjceny Matrix To Ajcency List
-        vector<int> adjList[V];
-        for(int i=0;i<V;i++){
-            for(int j=0;j<V;j++){
-                if(isConnected[i][j]==1 &&  i!=j){
-                     adjList[i].push_back(j);
-                     adjList[j].push_back(i); 
+        //Matrix --> Adjency List
+        int v = isConnected.size();
+        vector<int>adj[v];
+        for(int i=0;i<v;++i){
+            for(int j=0;j<v;++j){
+                if(isConnected[i][j]==1 && i!=j){
+                      adj[i].push_back(j);
+                      adj[j].push_back(i);
                 }
             }
         }
-        int vis[210]={0};
+        vector<int>vis(v,0);
         int count=0;
-        for(int i=0;i<V;i++){
-            if(!vis[i]){
-                count++;
-                dfs(adjList,vis,i);
-            }
+        for(int i=0;i<v;++i){
+             if(!vis[i]){
+                 dfs(i,adj,vis);
+                 count++;
+             }
         }
         return count;
-    }
+    }  
 };
+
+// //vector<int>adj[] (Can Store vector of array) = vector<vector<int>>&adj
+// class Solution {
+// private:
+//     //Here I not pass 3rd Argument Becasue my task in only count not print dfs.
+//     void dfs(int node,vector<vector<int>>&adj,vector<int>&vis){
+//         vis[node]=1;
+//         for(auto it : adj[node]){
+//             if(!vis[it]){
+//                 dfs(it,adj,vis);
+//             }
+//         }
+//     }
+    
+// public:
+   
+//     int findCircleNum(vector<vector<int>>& isConnected) {
+//         int V = isConnected.size();
+//         // vector<int> adj(V);
+//         vector<vector<int>> adj(V);
+        
+//         for(int i=0;i<V;i++){
+//             for(int j=0;j<V;j++){
+//                 if(i!=j && isConnected[i][j]==1){
+//                     adj[i].push_back(j);
+//                     adj[j].push_back(i);
+//                 }
+//             }
+//         }
+        
+//         int count=0;
+//         vector<int>vis(0);
+//         for(int i=0;i<V;i++){
+//             if(!vis[i]){
+//                   dfs(i,adj,vis);
+//                   count++;
+//             }     
+//         }
+//         return count;
+//     }
+// };
