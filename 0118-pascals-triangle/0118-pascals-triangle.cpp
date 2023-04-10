8a9c328005c8838e7d69f3bb20c2b95bc52bb8c6
Soni Neel NikunjKumar <90117961+neel13062003@users.noreply.github.com>
2023-04-10 22:45:37 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (52.28%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -22,3 +22,15 @@ public:
         return output;
     }
 };
+
+//https://workat.tech/problem-solving/approach/pt/pascals-triangle
+/*vector<int> pascalTriangleRow(int rowNo) {
+   vector<int> pascalRow;
+   pascalRow.push_back(1);
+   rowNo--;
+   for (int i = 1; i <= rowNo; i++) {
+       int rowElement = pascalRow.back() * (rowNo - i + 1) / i;
+       pascalRow.push_back(rowElement);
+   }
+   return pascalRow;
+}*/
